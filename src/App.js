7c0304e9a7a7f74d6bc08de319dbe4426c1833d7
Phong Ne <PHONGCENA\PhongCENA>
2023-06-21 10:44:50 +0700

    Hiển thị dữ liệu ra bảng

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
+import { useEffect, useState } from 'react';
 import './App.scss';
 import Header from './Components/Header';
 import TableUser from './Components/TableUser';
+import { fetchApiUser } from './services/UserService';
+import { Container } from 'react-bootstrap';
 
 function App() {
+  const [list, setList] = useState([])
+
+  useEffect(() => {
+    getAllUser()
+  }, [])
+
+  const getAllUser = async () => {
+    let res = await fetchApiUser()
+    console.log('>>> Check api: ', res);
+    if (res && res.data) {
+      setList(res.data.data)
+    }
+  }
   return (
     <div className='app-container'>
       <Header />
-      <TableUser />
+      <Container>
+        <TableUser
+          list={list}
+        /></Container>
     </div>
   );
 }
--- src/Components/TableUser.js
+++ src/Components/TableUser.js
@@ -1,8 +1,32 @@
+import Table from 'react-bootstrap/Table';
+
 const TableUser = (props) => {
+    let { list } = props
+    console.log(list);
     return (
-        <div>
-            Table User ....
-        </div>
+        <Table striped bordered hover>
+            <thead>
+                <tr>
+                    <th>Id</th>
+                    <th>Email</th>
+                    <th>First Name</th>
+                    <th>Last Name</th>
+
+                </tr>
+            </thead>
+            <tbody>
+                {list && list.length > 0 && list.map((item, index) => {
+                    return (
+                        <tr key={index}>
+                            <td>{item.id}</td>
+                            <td>{item.email}</td>
+                            <td>{item.first_name}</td>
+                            <td>{item.last_name}</td>
+                        </tr>
+                    )
+                })}
+            </tbody>
+        </Table>
     )
 }
 
